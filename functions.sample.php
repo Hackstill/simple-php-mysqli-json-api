41ac7fbd3994d3a9dd25248742eee5c73794e48a
Jehy <fate@jehy.ru>
2014-04-29 13:14:50 +0400

    fixed api link example in comment

--- functions.sample.php
+++ functions.sample.php
@@ -1,26 +1,27 @@
 <?
 /*for arg types:
 
-i corresponding variable has type integer 
-d corresponding variable has type double 
-s corresponding variable has type string 
-b corresponding variable is a blob and will be sent in packets 
+i corresponding variable has type integer
+d corresponding variable has type double
+s corresponding variable has type string
+b corresponding variable is a blob and will be sent in packets
 
 */
 $functions = array(
   'sample_func_1' => array(
-    'args' => array('user_id'),
+    'args' => array('node_id'),
     'arg_types' => 'i',#write arg types in string, for example 'iddsd'
     'arg_cmp'=>'>',
     'fields' => '*',
-    'tables' => 'dba_users',
+    'tables' => 'tree',
     'where' => '',
     'connect_name' => 'HOME',),
 );
+
 
 #sample authorization
 if ($_REQUEST['token'] !== '123')
   show_error('Error: Access error');
 
-#http://localhost/simple_api/index.php?func_name=sample_func_1&user_id=265&hash=123
+#http://localhost/simple_api/index.php?func_name=sample_func_1&node_id=17&token=123
 ?>
